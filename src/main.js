37c4d84d0f22833765a9472d49a14465d67654e0
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-04-26 17:59:48 +0200

    .

--- src/main.js
+++ src/main.js
@@ -3,20 +3,37 @@ var app = new Vue({
 el: '#root',
 data:{
   headerLinks: headerLinks,
-//  count1: 0,
-//  count2: 0,
-//  count3: 0,
-//  count4: 0,
+  index: 0,
+  sliders: [
+    {
+    immagine: "src/img/testimonial.webp",
+    frase: "“No man but feels more of a man in the world if he have but a bit of ground that he can call his own. However small it is on the surface, it is four thousand miles deep; and that is a very handsome property.”",
+    nome: "HARRY SMITH • NEW HOME OWNER",
+    },
+    {
+      immagine: "src/img/testimonial-2.webp",
+      frase: "“Many novice real estate investors soon quit the profession and invest. When you invest in real estate, you often see a side of humanity that stocks, bonds, mutual funds, and saving money shelter you from.”",
+      nome: "JOHN DOE • PROPERTY INVESTOR",
+    },
+  ],
 },
-  methods: {
-//    counter: function(){
-//      for (var i = 0; i < 3534; i++) {
-//        delay(this.count1)
-//      }
-//    },
-//    delay: function(){
-//      setTimeout(() =>
-//      this.count1, 1000);
-//    }
+methods: {
+  next: function (){
+    if (this.index < this.sliders.length - 1) {
+      this.index++;
+    } else {
+      this.index = 0;
+    }
+  },
+  prev: function (){
+    if (this.index > 0) {
+      this.index--;
+    } else {
+      this.index = this.sliders.length - 1;
+    }
+  },
+  dots: function(index) {
+    this.index = index;
   }
+}
 })
