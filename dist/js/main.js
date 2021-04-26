d6dbfe33ce75619be8e45f60d1e357d9edf1d068
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-04-26 14:58:12 +0200

    quasi finito

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,9 +11,22 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-    headerLinks: headerLinks
+    headerLinks: headerLinks //  count1: 0,
+    //  count2: 0,
+    //  count3: 0,
+    //  count4: 0,
+
   },
-  methods: {}
+  methods: {//    counter: function(){
+    //      for (var i = 0; i < 3534; i++) {
+    //        delay(this.count1)
+    //      }
+    //    },
+    //    delay: function(){
+    //      setTimeout(() =>
+    //      this.count1, 1000);
+    //    }
+  }
 });
 
 /***/ }),
--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ var app = new Vue({
 el: '#root',
 data:{
   headerLinks: headerLinks,
+//  count1: 0,
+//  count2: 0,
+//  count3: 0,
+//  count4: 0,
 },
-methods: {
-
-}
+  methods: {
+//    counter: function(){
+//      for (var i = 0; i < 3534; i++) {
+//        delay(this.count1)
+//      }
+//    },
+//    delay: function(){
+//      setTimeout(() =>
+//      this.count1, 1000);
+//    }
+  }
 })
